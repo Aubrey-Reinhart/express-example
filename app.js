12e6a48aff12698e812e65354cd7c56f76a658b3
Aubrey Reinhart <aubrey.reinhart@gmail.com>
2022-04-24 20:10:37 -0700

    Added access and data.json file of recipes

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/')
+app.get('/', (_, res) => {
+    res.send('Express App');
+});
+
+app.listen(3000, () => {
+    console.log(`server is running on port: ${port}`);
+});
